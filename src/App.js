0f64a2ab9c19ade1794787066dd3dc044b6fb33b
Remigio Fernandez <rfernandez@nisum.com>
2019-01-10 18:56:40 -0300

    Create person components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,125 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Person from './components/Person/Person'
 import './App.css';
+import {library} from '@fortawesome/fontawesome-svg-core'
+import * as Icon from '@fortawesome/free-solid-svg-icons'
+
+library.add(Icon.faArrowAltCircleDown)
+library.add(Icon.faArrowAltCircleUp)
+library.add(Icon.faTimesCircle)
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        persons: [
+            {
+                id: "wqe12",
+                name: "Remigio",
+                age: 24
+            },
+            {
+                id: "qwq312",
+                name: "Javier",
+                age: 23
+            }
+        ],
+        showPersons: false
+    }
+
+    switchNameHandler = () => {
+        this.setState({
+            persons:
+                [
+                    {
+                        id: "gq312",
+                        name: "Javier",
+                        age: 23
+                    },
+                    {
+                        id: "qdw312",
+                        name: "Remigio",
+                        age: 25
+                    },
+                    {
+                        id: "qwn312",
+                        name: "Fernandez",
+                        age: 26
+                    }
+                ]
+        })
+    }
+
+    deletePersonHandler = (idx) => {
+        const persons = this.state.persons;
+        persons.splice(idx, 1)
+        this.setState({
+            persons: persons
+        })
+    }
+
+    nameChangeHandler = (event,id) => {
+        const personIdx = this.state.persons.findIndex(p =>{
+            return p.id === id;
+        })
+
+        const person = {...this.state.persons[personIdx]};
+        //const person = Object.assign({},this.state.persons[personIdx])
+
+        person.name = event.target.value;
+
+        const persons = [...this.state.persons];
+        persons[personIdx] = person;
+        this.setState({
+            persons:persons
+        })
+    }
+
+    tooglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({
+            showPersons: !doesShow
+        })
+    }
+
+    showHandler = () => {
+        this.setState({
+            showPersons: true
+        })
+    }
+
+    render() {
+        const style = {
+            cursor: 'pointer',
+            color: '#131',
+            font: 'inherit',
+            border: '1px solid blue',
+            background: '#afa',
+            padding: '8px',
+            margin: '10px'
+        }
+
+        let listPerson = null
+
+        if (this.state.showPersons) {
+            listPerson = (<div> {this.state.persons.map((person, idx) =>
+                <Person click={() => this.deletePersonHandler(idx)}
+                         change={(event)=>this.nameChangeHandler(event,person.id)}
+                         name={person.name}
+                         age={person.age}
+                         key={person.id}/>)}
+            </div>)
+            style.background = '#faa'
+        }
+
+        return (
+            <div className="App">
+                <button style={style}
+                        onClick={this.tooglePersonsHandler}>
+                    Switch Names
+                </button>
+                {listPerson}
+            </div>
+        );
+    }
 }
 
 export default App;
